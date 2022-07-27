0d83a7182fe85219be0abb6decf7b7a41abd69d9
Vitienzo Capeletti <vitienzo.capeletti@southsystem.com.br>
2022-07-27 04:46:13 -0300

    feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
@@ -1,12 +1,19 @@
 package com.devsuperior.hrpayroll.service;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclient.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
+    public Payment getPayment(long workerId, int days) {
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
